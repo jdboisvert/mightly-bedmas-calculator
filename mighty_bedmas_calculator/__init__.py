d537b727a53631c90cfdb26320cbcbb58bd9ccc5
jdboisvert <info.jeffreyboisvert@gmail.com>
2022-07-24 23:41:45 -0400

    [RELEASE] 2022.7.25.1

--- mighty_bedmas_calculator/__init__.py
+++ mighty_bedmas_calculator/__init__.py
@@ -1,15 +1,11 @@
 # -*- coding: utf-8 -*-
 
-"""Odyssey Settings: Manage Odyssey settings without manually editing JSON.."""
-
 __author__ = """Jeffrey Boisvert"""
 __email__ = "info.jeffreyboisvert@gmail.com"
 
 from . import infix_evaluator
 from . import operator
 
-# any functions from backend you want to expose should be
-# imported above and added to the list below.
 __all__ = [
     "infix_evaluator",
     "operator"
